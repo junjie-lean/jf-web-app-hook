f77236ad6a135cb91dd3039a3ef75805f3e0fcad
李峻杰 <junjie.lean@foxmail.com>
2021-11-17 11:38:24 +0800

    增加自定义hook的文档输出

--- src/exports.js
+++ src/exports.js
@@ -2,14 +2,18 @@
  * @Author: junjie.lean
  * @Date: 2021-11-16 16:43:47
  * @Last Modified by: junjie.lean
- * @Last Modified time: 2021-11-17 09:34:26
+ * @Last Modified time: 2021-11-17 11:37:09
  */
 
 //对文档工具暴露需要生成开发文档的组件
 
 export * from './view/registered';
 
-export * from './util/util-fn'
-export * from './util/util-encrypt'
-export * from './util/util-request'
-export * from './util/util-outInterval'
+export * from './util/util-fn';
+export * from './util/util-encrypt';
+export * from './util/util-request';
+export * from './util/util-outInterval';
+
+export * from './hooks/useAsync';
+export * from './hooks/useKeyPress';
+export * from './hooks/useSyncState';
